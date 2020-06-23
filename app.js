795a262d7cf7ef852a9d4cb6a7f11eab97acdd01
reza <rezasemyari1991@gmail.com>
2020-06-23 20:10:34 +0430

    add subtract of calculate function

--- app.js
+++ app.js
@@ -9,9 +9,14 @@ $(document).on("click", "[id^=increase]", function () {
     
     //increase count of food
     let countFood = +($(`#countFood${num}`).text());
+    
     countFood++;
     $(`#countFood${num}`).text(countFood);
-    
+    //show minus sign
+    $(`#subtract${num}`).removeClass('d-none');
+    if(countFood > 0){
+        $(`#subtract${num}`).show();
+    }
     // calulate of price
     let unitPrice = +($(`#unitPrice${num}`).text());
     let price = unitPrice * countFood;
@@ -19,5 +24,26 @@ $(document).on("click", "[id^=increase]", function () {
  
 });
 
+// --------- When Decrease Count Of Food ----------
 
+$(document).on("click", "[id^=subtract]", function () {
+    
+    //get id selected
+    let decreaseId = $(this).attr('id');
+    let num = decreaseId.slice(-2);
+    
+    //decrease count of food
+    let countFood = +($(`#countFood${num}`).text());
+    countFood--;
+    $(`#countFood${num}`).text(countFood);
+    //hide minus sign
+    if(countFood <= 0){
+        $(`#subtract${num}`).hide();
+    }
+    // calulate of price
+    let unitPrice = +($(`#unitPrice${num}`).text());
+    let price = unitPrice * countFood;
+    $(`#price${num}`).text(price);
+ 
+});
 
