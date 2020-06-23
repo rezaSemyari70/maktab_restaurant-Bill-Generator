1e6ce25952eb641615aab8cf75829ac354ff3045
reza <rezasemyari1991@gmail.com>
2020-06-23 20:34:41 +0430

    add total order of caculate function

--- app.js
+++ app.js
@@ -2,48 +2,59 @@
 
 // --------- When Increse Count Of Food ----------
 $(document).on("click", "[id^=increase]", function () {
-    
+
     //get id selected
     let increaseId = $(this).attr('id');
     let num = increaseId.slice(-2);
-    
+
     //increase count of food
     let countFood = +($(`#countFood${num}`).text());
-    
+
     countFood++;
     $(`#countFood${num}`).text(countFood);
     //show minus sign
     $(`#subtract${num}`).removeClass('d-none');
-    if(countFood > 0){
+    if (countFood > 0) {
         $(`#subtract${num}`).show();
     }
     // calulate of price
     let unitPrice = +($(`#unitPrice${num}`).text());
     let price = unitPrice * countFood;
     $(`#price${num}`).text(price);
- 
+    totalOrder()
 });
 
 // --------- When Decrease Count Of Food ----------
 
 $(document).on("click", "[id^=subtract]", function () {
-    
+
     //get id selected
     let decreaseId = $(this).attr('id');
     let num = decreaseId.slice(-2);
-    
+
     //decrease count of food
     let countFood = +($(`#countFood${num}`).text());
     countFood--;
     $(`#countFood${num}`).text(countFood);
     //hide minus sign
-    if(countFood <= 0){
+    if (countFood <= 0) {
         $(`#subtract${num}`).hide();
     }
     // calulate of price
     let unitPrice = +($(`#unitPrice${num}`).text());
     let price = unitPrice * countFood;
     $(`#price${num}`).text(price);
- 
+    totalOrder()
 });
 
+// --------- Total Orders ----------
+
+function totalOrder() {
+    let totalOrders = 0;
+    $.each($('[id^=price]'), function (index, element) {
+        totalOrders += +($(this).text());
+        console.log(totalOrders);
+    });
+
+    $('#totalOrder').text(totalOrders);
+}
